85c00526e05de3e8794104955e8924c15d83fb3d
krishnanpalay <krishnanparanjothi@gmail.com>
2019-08-02 14:45:02 +0530

    Added New IP Address in APP.js

--- js/app.js
+++ js/app.js
@@ -36,10 +36,10 @@ angular
   return $rootScope.$stateParams = $stateParams;
 }]).run(['$rootScope',function($rootScope) {
 
-    $rootScope.HOST_TMS = "http://172.16.0.229:8080/TMS/";
+    $rootScope.HOST_TMS5 = "http://172.16.0.229:8080/TMS/";
     $rootScope.HOST_TMS1 = "https://tpms.placer.in/TMS/";
-    $rootScope.HOST_TMS1 = "https://tpms-api.placer.in/TMS/";
-    $rootScope.HOST_TMS1 = "https://intranet.group10.in/CANtrackerAPI/";   //testing server
+    $rootScope.HOST_TMS = "https://tpms-api.placer.in/TMS/";
+    $rootScope.HOST_TMS3= "https://intranet.group10.in/CANtrackerAPI/";   //testing server
     
     $rootScope.rootSelectedDeportId = 0;
 
