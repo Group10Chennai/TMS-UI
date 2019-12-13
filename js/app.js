9dc5ef5e590b3f0dceb12525a888a65133be80a8
krishnanpalay <krishnanparanjothi@gmail.com>
2019-12-13 14:17:21 +0530

    add tyre details

--- js/app.js
+++ js/app.js
@@ -35,9 +35,9 @@ angular
   $rootScope.$state = $state;
   return $rootScope.$stateParams = $stateParams;
 }]).run(['$rootScope',function($rootScope) {
-  $rootScope.HOST_TMS1111122 = "http://172.16.0.229:8080/TMS/";
+  $rootScope.HOST_TMS111111111111111 = "http://172.16.0.229:8080/TMS/"; //Ram Manni Machine
   $rootScope.HOST_TMS1 = "https://tpms.placer.in/TMS/";
-  $rootScope.HOST_TMS = "https://tpms-api.placer.in/TMS/";
+  $rootScope.HOST_TMS = "https://tpms-api.placer.in/TMS/";//Live Server
   $rootScope.HOST_TMSdddddddddd= "https://intranet.group10.in/CANtrackerAPI/";
     
     $rootScope.rootSelectedDeportId = 0;
